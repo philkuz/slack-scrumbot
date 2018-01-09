5323cafad3e82c667916a704cb70e78b86510cbd
philkuz <superpker@gmail.com>
2018-01-08 20:44:39 -0800

    Adding scripts to setup and run scrumbot

---
+++ setup_scrumbot.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/smhg/slack-scrumbot
+cd slack-scrumbot
+npm install -g forever
+npm install 
---
+++ start_scrumbot.sh
@@ -0,0 +1,1 @@
+DEBUG=slack-scrumbot* SLACK_LOG_LEVEL=debug SCRUMBOT_TOKEN=xoxb-295965414932-BQ4f3HP0uDNplwzS7eqwdCjX npm run watch
